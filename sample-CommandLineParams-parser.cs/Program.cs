2606218f354e8410184cf46ed7299ba6a5e2bd79
osawa-koki <github@osawa.dev>
2022-11-20 02:09:46 +0900

    Mainコード。

--- sample-CommandLineParams-parser.cs/Program.cs
+++ sample-CommandLineParams-parser.cs/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.AddCommandLine(args);
+
+var config = builder.Build();
+
+Console.WriteLine($"Key1: '{config["Key1"]}'");
+Console.WriteLine($"Key2: '{config["Key2"]}'");
+Console.WriteLine($"Key3: '{config["Key3"]}'");
+Console.WriteLine($"Key4: '{config["Key4"]}'");
+Console.WriteLine($"Key5: '{config["Key5"]}'");
